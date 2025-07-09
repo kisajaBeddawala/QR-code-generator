b68a058ea68ba800be40b16b7eff703b426d550e
kisajaBeddawala <kisajab72@gmail.com>
2025-07-09 10:56:23 +0530

    download qr code

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 document.getElementById("btn").addEventListener("click",function(e){
     e.preventDefault();
     let data = document.getElementById("txt").value;
-    document.getElementById("qr-code").innerHTML = "";
 
-    new QRCode(document.getElementById("qr-code"),{
-        text : data,
-        width : 150,
-        height : 150,
-        colorDark : "#f90000",
-        colorLight : "#ffffff",
-    });
+    if(data === ""){
+        alert("Please enter some text to generate QR code");
+    }else{
+        new QRCode(document.getElementById("qr-code"),{
+            text : data,
+            width : 150,
+            height : 150,
+            colorDark : "#f90000",
+            colorLight : "#ffffff",
+        });
+    
+        document.getElementById("qr-code").style.border = "2px solid black";
+        document.getElementById("qr-download").style.display = "block";
+    }
 
-    document.getElementById("qr-code").style.border = "2px solid black";
+    document.getElementById("qr-download").addEventListener("click",function(){
+        let qrImg = document.querySelector("#qr-code img");
+        let link = document.createElement("a");
+        link.href = qrImg.src;
+        link.download = `${data}.png`;
+        link.click();
+    })
 })
+
